0531e0122d58e4f38cb2ceffdb74f07d9b9ea2bd
Liang <liliang.li@yahoo.com>
2015-11-09 09:30:16 -0500

    added mongodb instance configuration
    bump up version to 0.0.6
    
    get customer by slug clean up

--- app/models/SocialNetworks.scala
+++ app/models/SocialNetworks.scala
@@ -1,7 +1,22 @@
 package models
 
+import play.api.libs.json.OWrites
+
 /**
 	* Created by lli on 11/6/15.
 	*/
 case class SocialNetwroks(facebook_username: String, twitter_username: String, instagram_username: String,
                           tumblr_username: String, google_username: String)
+
+object SocialNetworks{
+	implicit object SocialNetworksWrites extends OWrites[SocialNetwroks] {
+		import play.api.libs.json._
+		def writes(sns: SocialNetwroks): JsObject = Json.obj(
+			"facebook_username" -> sns.facebook_username,
+			"twitter_username" -> sns.twitter_username,
+			"instagram_username" -> sns.instagram_username,
+			"tumblr_username" -> sns.tumblr_username,
+			"google_username" -> sns.google_username
+		)
+	}
+}
---
+++ app/utils/MongoDBValidationHelper.scala
@@ -0,0 +1,12 @@
+package utils
+
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.bson.BSONDocument
+import utils.PostUserDataHelper.Customer
+
+/**
+  * Created by liliangli on 11/9/15.
+  */
+object MongoDBValidationHelper {
+
+}
